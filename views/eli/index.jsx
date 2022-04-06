dd9e2f14a9ad94a3876ccbd556c631117757a66b
EliDelMal <99223723+EliDelMal@users.noreply.github.com>
2022-04-05 22:07:32 -0500

    eliminé yarn.lock

---
+++ views/eli/index.jsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+
+const EliDelMal= () => {
+    return (
+        <View>
+            <Text> Hola mundo mi nombre es Eli </Text>
+        </View>
+    );
+};
+
+export {EliDelMal}
