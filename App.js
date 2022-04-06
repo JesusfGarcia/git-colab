77917e28e3c825f2e48595c69f528f88da59a51f
ViriPineda <ada_hack420@hackademy.mx>
2022-04-05 22:42:14 -0500

    agregue mi vista

--- App.js
+++ App.js
@@ -5,23 +5,24 @@ import { Miri } from './views/miri/miri';
 import { Mayra } from './views/mayra';
 import { Karyrodriguez } from './views/karyrodriguez';
 import { EliDelMal } from './views/eli';
+import { Viripineda } from './views/viripineda';
 
-const arraytorender=[ <Pazanaya/>, <Miri/>, <Mayra/>, <Karyrodriguez/>, <EliDelMal/>]
+const arraytorender = [ < Pazanaya / > , < Miri / > , < Mayra / > , < Karyrodriguez / > , < EliDelMal / > , < Viripineda / > ]
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {arraytorender.map(componente => componente)}
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( <
+        View style = { styles.container } > { arraytorender.map(componente => componente) } <
+        StatusBar style = "auto" / >
+        <
+        /View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
