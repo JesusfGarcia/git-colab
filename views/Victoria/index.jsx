73ec4913583f78f54612ff8ec6978f1d266a0bb2
Victoria Morones <victoria.morones@gmail.com>
2022-04-05 23:39:35 -0500

    Aqregue nuevo componente

---
+++ views/Victoria/index.jsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+
+const Victoriam= () => {
+    return (
+        <View>
+            <Text> Hola mundo mi nombre es Victoria</Text>
+        </View>
+    );
+};
+
+export {Victoriam}
