b1529f4559b6ef445aeb0f6b23dcb035d0fa14d0
mrivh <m.alicia.riv@gmail.com>
2022-04-05 21:56:38 -0500

    Add: branch and jsx

---
+++ views/miri/miri.jsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+
+const Miri= () => {
+    return (
+        <View>
+            <Text> Hello! Fellows! </Text>
+        </View>
+    );
+};
+
+export {Miri}
