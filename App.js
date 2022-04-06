62e01bb7b2b7bc01e8ace364d52528bb923ab6f7
pazanaya <ada_hack329@hackademy.mx>
2022-04-05 22:34:39 -0500

    se creo un array de componentes

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Pazanaya } from './views/pazanaya';
+import { Miri } from './views/miri/miri';
+import { Mayra } from './views/mayra';
+import { Karyrodriguez } from './views/karyrodriguez';
+import { EliDelMal } from './views/eli';
+
+const arraytorender=[ <Pazanaya/>, <Miri/>, <Mayra/>, <Karyrodriguez/>, <EliDelMal/>]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {arraytorender.map(componente => componente)}
       <StatusBar style="auto" />
     </View>
   );
