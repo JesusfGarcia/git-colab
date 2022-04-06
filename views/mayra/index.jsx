23351d54f28bd09f289a4d21f68d3a92541c3f81
Mayra <ada_hack242@hackademy.mx>
2022-04-05 22:06:20 -0500

    cambie el nombre

---
+++ views/mayra/index.jsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+
+const Mayra= () => {
+    return (
+        <View>
+            <Text> Hola mundo mi nombre es Mayra Martínez </Text>
+        </View>
+    );
+};
+
+export {Mayra}
