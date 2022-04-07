8a183e352360794c04a76c8e721d311333494d43
Victoria Morones <victoria.morones@gmail.com>
2022-04-06 22:07:13 -0500

    Se agregan estilos

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Pazanaya } from './views/pazanaya';
-import { Miri } from './views/miri/miri';
-import { Mayra } from './views/mayra';
-import { Karyrodriguez } from './views/karyrodriguez';
-import { EliDelMal } from './views/eli';
-import { Viripineda } from './views/viripineda';
-import { FridaGeyne } from './views/FridaGeyne';
-import { Belendiaz } from './views/belendiaz';
-import { Victoriam } from './views/Victoria';
-const arraytorender=[ <Pazanaya/>, <Miri/>, <Mayra/>, <Karyrodriguez/>, <EliDelMal/>, <Viripineda/>, <FridaGeyne/>, <Belendiaz/>, <Victoria/>];
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Text } from "./components";
+import { Pazanaya } from "./views/pazanaya";
+import { Miri } from "./views/miri/miri";
+import { Mayra } from "./views/mayra";
+import { Karyrodriguez } from "./views/karyrodriguez";
+import { EliDelMal } from "./views/eli";
+import { Viripineda } from "./views/viripineda";
+import { FridaGeyne } from "./views/FridaGeyne";
+import { Belendiaz } from "./views/belendiaz";
+import { Victoriam } from "./views/Victoria";
+const arraytorender = [
+  <Pazanaya />,
+  <Miri />,
+  <Mayra />,
+  <Karyrodriguez />,
+  <EliDelMal />,
+  <Viripineda />,
+  <FridaGeyne />,
+  <Belendiaz />,
+  <Victoriam />,
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {arraytorender.map(componente => componente)}
-      <StatusBar style="auto" />
+      <View style={styles.vista1}>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
+      </View>
+      <View style={styles.vista2}>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
+      </View>
+      <View style={styles.vista3}>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
@@ -23,8 +47,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    flexDirection: "row-reverse",
+  },
+  vista1: {
+    backgroundColor: "#337AFF",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  vista2: {
+    backgroundColor: "#E9FF33",
+    flex: 2,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  vista3: {
+    backgroundColor: "#9033FF",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+  },
+  text: {
+    fontSize: 40,
   },
 });
