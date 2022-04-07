d2c781cc30ae6e7d66562570b050200d931577ce
pazanaya <ada_hack329@hackademy.mx>
2022-04-06 22:05:55 -0500

    se trabajo con estilos

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from "./components"
 import { Pazanaya } from './views/pazanaya';
 import { Miri } from './views/miri/miri';
 import { Mayra } from './views/mayra';
@@ -9,13 +10,28 @@ import { Viripineda } from './views/viripineda';
 import { FridaGeyne } from './views/FridaGeyne';
 import { Belendiaz } from './views/belendiaz';
 import { Victoriam } from './views/Victoria';
-const arraytorender=[ <Pazanaya/>, <Miri/>, <Mayra/>, <Karyrodriguez/>, <EliDelMal/>, <Viripineda/>, <FridaGeyne/>, <Belendiaz/>, <Victoria/>];
+
+const arraytorender=[ <Pazanaya/>, <Miri/>, <Mayra/>, <Karyrodriguez/>, <EliDelMal/>, <Viripineda/>, <FridaGeyne/>, <Belendiaz/>, <Victoriam/>];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {arraytorender.map(componente => componente)}
-      <StatusBar style="auto" />
+      <View style={styles.vista1}>
+        <Text style={styles.text}>1</Text>
+        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>3</Text>
+      </View>
+      <View style={styles.vista2}>
+        <Text style={styles.text}>1</Text>
+        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>3</Text>
+      </View>
+      <View style={styles.vista3}>
+        <Text style={styles.text}>1</Text>
+        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>3</Text>
+      </View>
+
     </View>
   );
 }
@@ -23,8 +39,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    flexDirection: "row-reverse",
   },
+  vista1: {
+    backgroundColor: '#f08080',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  vista2: {
+    backgroundColor: '#e9ff33',
+    flex: 2,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  vista3: {
+    backgroundColor:'#9033ff',
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+  },
+  text: {
+    fontSize: 40,
+  }
+
 });
