93a1658e5507d07e001d4c1365662dfc80587d5f
ViriPineda <ada_hack420@hackademy.mx>
2022-04-12 20:58:12 -0500

    casi lito

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Picker } from "react-native";
 import { Text, MyTextInput, MyBoton } from "./components";
 import { TextInput } from "react-native";
 import React from "react";
-import Icon from "react-native-vector-icons/FontAwesome"
+import DropDownPicker from "react-native-dropdown-picker";
 
 export default function App() {
   const [Correo, setCorreo] = React.useState("");
   const [Contraseña, setContraseña] = React.useState("");
-  const [ConfContraseña, setConfContraseña] = React.useState("");
+  const [TipoUsuario, setTipoUsuario] = React.useState("default");
+  const [OnePicker, setOnePicker] = React.useState(false);
+  const [PickerItems, setPickerItems] = React.useState([
+    {
+      label: "solicitante",
+      value: "solicitante",
+    },
+    {
+      label: "empleador",
+      value: "empleador",
+    },
+  ]);
 
   const enviarFormulario = () => {
     if (Correo === "") {
@@ -17,35 +28,19 @@ export default function App() {
     if (Contraseña === "") {
       return alert("Necesita llenar el campo de Contraseña");
     }
-    if (ConfContraseña === "") {
-      return alert("Necesita llenar el campo de Confirma tu Contraseña");
-    }
-    if (ConfContraseña != Contraseña) {
-      return alert("Usuario y/o contraseña invalido");
-    }else
     return alert("El formulario se ha llenado de manera correcta");
-
-
-
   };
-
   return (
     <View style={styles.container}>
       <Text>
-        REGISTRO DE USUARIO
+        INICIAR SESIÓN
       </Text>
-
-      
-
       <MyTextInput
         label="Correo electrónico"
-        place="e.g. name@gmail.com"
+        place="Ingresa tu Correo Electrónico"
         value={Correo}
         setValue={setCorreo}
       />
-      
-      <View style = {styles.theeye}>
-      
       <MyTextInput
         label="Contraseña"
         place="Ingresa tu Contraseña"
@@ -53,20 +48,7 @@ export default function App() {
         value={Contraseña}
         setValue={setContraseña}
       />
-      <Icon 
-      size = {30}
-      color = {"#000"}
-      name = {"eye"}
-       />
-      </View>
-      <MyTextInput
-        label="Confirma tu Contraseña"
-        place="Ingresa tu Contraseña de nuevo"
-        security
-        value={ConfContraseña}
-        setValue={setConfContraseña}
-        
-      />
+      <DropDownPicker open = {OnePicker} value = {TipoUsuario} items = {PickerItems} setOpen = {setOnePicker} setValue = {setTipoUsuario} setItems = {setPickerItems} />
       <View style={styles.miBotonContener}>
         <MyBoton text="aceptar" onPress={enviarFormulario} />
       </View>
@@ -82,11 +64,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 60,
     justifyContent: "space-evenly",
   },
-
-  theeye:{
-    flexDirection: "row",
-  },
-
   miBotonContener: {
     flexDirection: "row",
     justifyContent: "space-between",
